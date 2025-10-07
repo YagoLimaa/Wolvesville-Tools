c161202932b31820cd51f67736ffc4ebde0d7562
Yago <yagolazevedo@gmail.com>
2025-10-07 10:05:56 -0300

    teste vite novo

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 import path from "path"
 
 // https://vitejs.dev/config/
-export default defineConfig({ plugins: [react()],
+export default defineConfig({
+  base: '/front/',
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
