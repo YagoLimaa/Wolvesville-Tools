15ee84d97eb015c778d24a251463ec20e32c4b8b
Yago <yagolazevedo@gmail.com>
2025-10-07 10:36:25 -0300

    teste definidto

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -3,22 +3,25 @@ import react from '@vitejs/plugin-react-swc'
 import path from "path"
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ command }) => {
+  return {
+    base: command === 'build' ? '/front/' : '/',
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-      },
-      '/images': {
-        target: 'http://localhost:3000',
-      },
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://localhost:3000',
+          changeOrigin: true,
+        },
+        '/images': {
+          target: 'http://localhost:3000',
+        },
+      }
     }
   }
 })
