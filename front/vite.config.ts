4a12fe55f9087928921c2d70f8dde999664b921a
Yago <yagolazevedo@gmail.com>
2025-10-13 12:45:43 -0300

    oi

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -5,7 +5,7 @@ import path from "path"
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
   return {
-    base: '/',
+    base: command === 'serve' ? '/' : './',
     plugins: [react()],
     resolve: {
       alias: {
