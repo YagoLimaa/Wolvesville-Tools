2c7f91b315bffc5e6254e4a7ed867ad63db16f31
Yago <yagolazevedo@gmail.com>
2025-10-13 12:14:16 -0300

    agr vai

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -5,7 +5,7 @@ import path from "path"
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
   return {
-    base: command === 'build' ? '/front/' : '/',
+    base: '/',
     plugins: [react()],
     resolve: {
       alias: {
