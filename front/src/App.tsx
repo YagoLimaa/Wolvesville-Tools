6c3fc1dedbc3f42ea72f2d192d3c6c61cbde15d5
Yago <yagolazevedo@gmail.com>
2025-11-02 13:07:40 -0300

    feat: acessos no site

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -15,38 +15,46 @@ import ClanInfo from "./pages/ClanInfo";
 import NotFound from "./pages/NotFound";
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Footer } from "./components/ui/Footer";
+import { useEffect } from "react";
+import mixpanel from "mixpanel-browser";
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <RolesProvider>
-      <ItemsProvider>
-        <TooltipProvider>
-          <Toaster />
-          <Sonner />
-          <BrowserRouter>
-            <div className="flex flex-col min-h-screen">
-              <main className="flex-grow">
-                <Routes>
-                  <Route path="/" element={<Index />} />
-                  <Route path="/search" element={<SearchPlayer />} />
-                  <Route path="/items/shop" element={<ItemsShop />} />
-                  <Route path="/clan/search" element={<ClanSearch />} />
-                  <Route path="/clan/rankings" element={<ClanRankings />} />
-                  <Route path="/clan/:id" element={<ClanInfo />} />
-                  <Route path="/items/skins" element={<ItemsSkins />} />
-                  <Route path="*" element={<NotFound />} />
-                </Routes>
-              </main>
-              <Footer />
-            </div>
-          </BrowserRouter>
-        </TooltipProvider>
-      </ItemsProvider>
-    </RolesProvider>
-  </QueryClientProvider>
-);
+const App = () => {
+  useEffect(() => {
+    mixpanel.track("Site Visited");
+  }, []);
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RolesProvider>
+        <ItemsProvider>
+          <TooltipProvider>
+            <Toaster />
+            <Sonner />
+            <BrowserRouter>
+              <div className="flex flex-col min-h-screen">
+                <main className="flex-grow">
+                  <Routes>
+                    <Route path="/" element={<Index />} />
+                    <Route path="/search" element={<SearchPlayer />} />
+                    <Route path="/items/shop" element={<ItemsShop />} />
+                    <Route path="/clan/search" element={<ClanSearch />} />
+                    <Route path="/clan/rankings" element={<ClanRankings />} />
+                    <Route path="/clan/:id" element={<ClanInfo />} />
+                    <Route path="/items/skins" element={<ItemsSkins />} />
+                    <Route path="*" element={<NotFound />} />
+                  </Routes>
+                </main>
+                <Footer />
+              </div>
+            </BrowserRouter>
+          </TooltipProvider>
+        </ItemsProvider>
+      </RolesProvider>
+    </QueryClientProvider>
+  );
+};
 
 export default App;
 
--- front/src/main.tsx
+++ front/src/main.tsx
@@ -2,5 +2,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import "./i18n";
+import mixpanel from "mixpanel-browser";
+
+mixpanel.init(import.meta.env.VITE_MIXPANEL_TOKEN, {
+  autocapture: false,
+  record_sessions_percent: 0,
+});
 
 createRoot(document.getElementById("root")!).render(<App />);
