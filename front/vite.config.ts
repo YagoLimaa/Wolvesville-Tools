cd382e45bc1bc1a1c3d36aa8720ab766c0efafdb
Yago <yagolazevedo@gmail.com>
2025-12-23 13:24:00 -0300

    fix: update sitemap generation timestamps and improve config structure

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -4,28 +4,31 @@ import path from "path"
 import sitemap from 'vite-plugin-sitemap'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  return {
-    base: '/',
-    plugins: [
-      react(),
-      sitemap({
-        hostname: 'https://wolvesville-tools.pages.dev',
-        robots: [{ userAgent: '*', allow: '/' }],
-        priority: 1.0,
-        dynamicRoutes: [
-          '/clans',
-          '/clan-rankings',
-          '/players',
-          '/items/shop',
-          '/items/skins'
-        ]
-      })
-    ],
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
-      },
+export default defineConfig({
+  base: '/',
+  plugins: [
+    react(),
+    sitemap({
+      hostname: 'https://wolvesville-tools.pages.dev',
+      dynamicRoutes: [
+        '/',
+        '/clans',
+        '/clan-rankings',
+        '/players',
+        '/items/shop',
+        '/items/skins'
+      ],
+      robots: [
+        {
+          userAgent: '*',
+          allow: '/',
+        },
+      ],
+    })
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-  }
+  },
 })
