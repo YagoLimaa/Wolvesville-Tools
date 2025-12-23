c1df180e338705e6f390e55f03f15374c02a33a7
Yago <yagolazevedo@gmail.com>
2025-12-23 12:19:15 -0300

    feat(seo): configure Google verification and sitemap generation

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from "path"
+import sitemap from 'vite-plugin-sitemap'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
   return {
     base: '/',
-    plugins: [react()],
+    plugins: [
+      react(),
+      sitemap({
+        hostname: 'https://wolvesville-tools.pages.dev',
+        robots: [{ userAgent: '*', allow: '/' }]
+      })
+    ],
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"),
